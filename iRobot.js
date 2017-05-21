1408a5739216b8d9fad2c91cade310eaa9de6642
Jose Luis Castillo <josecastillocs@gmail.com>
2017-05-20 23:02:35 -0500

    Uploading more small layout changes

--- iRobot.js
+++ iRobot.js
@@ -3,5 +3,13 @@
 	Date of creation: 5/20/2017-->
 
 <html>
-	
+	<head>
+		<title>iRobot - A Virtual personal robot</title>
+	</head>
+
+	<body>
+		<div style="margin-top:100px; text-align:center">
+			<img id="rockImg" src="iRobotFace.png" alt="iRobot" />
+		</div>
+	</body>
 </html>	
